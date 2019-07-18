197f2516dcade7ba69931baf9502902ccc0325dd
hjhjcallme1 <hjhjcallme1@gmail.com>
2019-07-19 02:16:22 +0700

    PlantUML-classdiagram

---
+++ demoPractice/demoPractice/AbstractAndInterface/collection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoPractice.AbstractAndInterface
+{
+    public abstract class Collection : ICollection
+    {
+        public int Count { get; set; }
+
+        public object SyncRoot { get; set; }
+
+        public bool IsSynchronized { get; set; }
+
+        public void CopyTo(Array array, int index)
+        {
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return null;
+        }
+    }
+}
